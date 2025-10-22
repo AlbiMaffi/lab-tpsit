5571c899b88e392153d9d71fc2eaf6aa0652e346
alberto <maffialberto07@gmail.com>
2025-10-22 09:20:32 +0200

    aggiornata_classe_moto

--- tpsit/Moto.cs
+++ tpsit/Moto.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace tpsit
 {
-    class Moto
+    public class Moto : Veicolo
     {
+        public int Cilindrata { get; set; }
+
+        public Moto(string marca, string modello, int anno, int cilindrata)
+            : base(marca, modello, anno)
+        {
+            Cilindrata = cilindrata;
+        }
+
+        public override void MostraDettagli()
+        {
+            Console.WriteLine($"Moto: {Marca} {Modello}, Anno: {Anno}, Cilindrata: {Cilindrata}cc");
+        }
     }
 }
